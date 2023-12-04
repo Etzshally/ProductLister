94c6050fc02caee1a7640a465c8b8d1fbf277bb1
Etzshally <shahpaal10@hotmail.com>
2023-12-04 17:53:13 +0500

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function about() {
+    return (
+        <>
+            <div className="bg-red-800 min-h-screen w-full">
+                <h2 className="text-center font-extrabold text-[40px]">
+                    About
+                </h2>
+            </div>
+        </>
+    )
+}
---
+++ src/app/products/[Id]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = ({params}) => {
+  return (
+    <div>product {params.Id} details</div>
+  )
+}
+
+export default page
