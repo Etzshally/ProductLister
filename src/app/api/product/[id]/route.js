e7d106f1bf80cc1287f93e7778326876057818cf
Etzshally <shahpaal10@hotmail.com>
2023-12-08 14:38:25 +0500

    `next apis`

--- src/app/api/product/[id]/route.js
+++ src/app/api/product/[id]/route.js
@@ -1,14 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-// import { promises as fs } from 'fs';
+import { promises as fs } from 'fs';
 
 export const GET = async (NextRequest) => {
-  const id  = NextRequest.url.split('product/')[1];
-  console.log("get id is",id)
-  return NextResponse.json(`Product: ${id}`)
+  const id = NextRequest.url.split('product/')[1];
+  const file = await fs.readFile(process.cwd() + '/public/data/items.json', 'utf8');
+  const {products} = JSON.parse(file);
+  const product = products && products.find((product) => product.id === parseInt(id, 10));
+  if (product) {
+    return NextResponse.json({ product },{status:200});
+  } else {
+    return NextResponse.json({ error: 'Product not found' }, { status: 404 });
+  }
 }
 
 export const DELETE = async (NextRequest) => {
-  const id  = NextRequest.url.split('product/')[1];
-  console.log("delete id is",id)
+  const id = NextRequest.url.split('product/')[1];
   return NextResponse.json(`Product: ${id}`)
 }
--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,9 +1,45 @@
 import React from 'react'
 
-const page = ({ params }) => {
-    // console.log(params)
+export const getProductDetails = async (id) => {
+    try {
+        const response = await fetch(`http://localhost:3000/api/product/${id}`, {
+            cache: 'no-store'
+        })
+        const parsed = await response.json()
+        return parsed
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const page = async ({ params }) => {
+    const { product } = await getProductDetails(params.id)
     return (
         <>
+            <div key={product.id} className="product-card">
+                <div className="product-details">
+                    <h2 className="text-white font-semibold product-name">{product.name}</h2>
+                    <p className="product-description">{product.description}</p>
+                    <p className="product-price">${product.price.toFixed(2)}</p>
+                    <p className="product-category">Category: {product.category}</p>
+                    <p className="product-brand">Brand: {product.brand}</p>
+                    <p className="product-color">Color: {product.color}</p>
+                    <p className={`product-availability ${product.availability ? 'available' : 'out-of-stock'}`}>
+                        Availability: {product.availability ? 'In Stock' : 'Out of Stock'}
+                    </p>
+                    <div className="product-rating">
+                        <p>Rating: {product.rating}</p>
+                        <div className="product-reviews">
+                            {product.reviews.map((review, index) => (
+                                <div key={index} className="review">
+                                    <p className="review-user">{review.user}</p>
+                                    <p className="review-comment">{review.comment}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
