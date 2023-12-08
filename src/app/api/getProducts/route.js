4d4d75afdfe8857b049e6643a879dc58f54c3306
Etzshally <shahpaal10@hotmail.com>
2023-12-08 14:44:40 +0500

    `convention`

--- src/app/api/getProducts/route.js
+++ src/app/api/getProducts/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { promises as fs } from 'fs';
 
-export async function GET(request) {
+export const GET = async (request) => {
   const file = await fs.readFile(process.cwd() + '/public/data/items.json', 'utf8');
   return NextResponse.json(JSON.parse(file))
 }
